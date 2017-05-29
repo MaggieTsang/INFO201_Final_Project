2c6462c8aeea516edb36817110724131d18793ea
Maggie Tsang <mbytsang@gmail.com>
2017-05-29 16:13:18 -0700

    GSM page

--- script/chart_year_sexualMinority.R
+++ script/chart_year_sexualMinority.R
@@ -1,1 +1,26 @@
-# Maggie chart - year vs sexual minority
+# Maggie chart
+# Views trends between year and the appearance frequency of sexual minorities
+
+library(dplyr)
+library(plotly)
+
+# Uses a line graph to view trendlines
+YearSexuality <- function(dataset) {
+  # Straight character information
+  straight.characters <- dataset %>% filter(GSM == "", is.na(YEAR) == FALSE) %>% group_by(YEAR) %>% summarise(freq = n())
+  colnames(straight.characters) <- c("YEAR", "str_freq")
+  
+  # Non-straight character information
+  gsm.characters <- dataset %>% filter(GSM != "", is.na(YEAR) == FALSE) %>% group_by(YEAR) %>% summarise(freq = n())
+  colnames(gsm.characters) <- c("YEAR", "gsm_freq")
+  
+  # Summarizes sexuality data, change N/A frequencies to zero
+  plot.summary <- left_join(straight.characters, gsm.characters, by = "YEAR")
+  plot.summary[is.na(plot.summary)] <- 0
+  
+  # Create and return graph with two trendlines
+  line.graph <- plot_ly(plot.summary, x = ~YEAR, y = ~str_freq, name = "Straight Characters", type = "scatter", mode = "lines") %>% 
+    add_trace(y = ~gsm_freq, name = "GSM Characters", type = "scatter", mode = "lines") %>%
+    layout(xaxis = list(title = "Year of First Appearance"), yaxis = list(title = "Character Frequency"), title = "Comparing Trends of Comic Character Sexual Minorities") %>%
+    return()
+}
--- server.R
+++ server.R
@@ -1,1 +1,22 @@
-#server
+# Server-side of webpage
+
+library(plotly)
+library(dplyr)
+
+marvel.data <- read.csv("data/marvel-wikia-data.csv", stringsAsFactors = FALSE)
+colnames(marvel.data)[13]  <- "YEAR"
+dc.data <- read.csv("data/dc-wikia-data.csv", stringsAsFactors = FALSE)
+source("./script/chart_year_sexualMinority.R")
+
+
+shinyServer(function(input, output) {
+  output$year_gsm <- renderPlotly({
+    # Show only selected gender. *** = all possible genders
+    gender.selected <- input$gender
+    if(gender.selected != "***") {
+      marvel.data <- marvel.data %>% filter(SEX == gender.selected)
+    }
+    return(MarvelYearSexuality(marvel.data))
+  })
+  
+})
